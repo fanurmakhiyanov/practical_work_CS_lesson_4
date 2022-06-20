08b74aa430e371973a3bc0a28864917f5d2b2804
fanur <makhiyanov.ff@gmail.com>
2022-06-20 16:30:29 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string numberStr = Console.ReadLine();
+    int number = int.Parse(numberStr);
+    return number;
+}
+int number = Prompt("Введите число: ");
+
+int Sum(int maxNumber)
+{
+    int result = 0;
+    for (int i = 0; i <= maxNumber; i++)
+    {
+        result = result + i;
+    }
+    return result;
+}
+System.Console.WriteLine($"Сумма цифр в числе {number} равняется {Sum(number)}");
