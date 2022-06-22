d2880399de25b9d5bde148f2f6f14985457731a2
fanur <makhiyanov.ff@gmail.com>
2022-06-22 17:22:07 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах).
+
+void PrintArray()
+{
+    int[] myArray  = new int[8];
+    int i = 0;
+    for (i = 0; i < 8; i++)
+    {
+        Random chaotic = new Random();
+        myArray[i] = chaotic.Next(10,100);
+        Console.Write($"{myArray[i]}, " );
+    }
+}
+
+PrintArray();
